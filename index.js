de65bf940a799fbb21ab87af1695216863941ae1
SUDHANSHU UNIYAL <uniyal1001@gmail.com>
2019-10-15 20:57:31 +0530

    javascript file for scripting
    file for functionality of website

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function fun(){
+var object=new XMLHttpRequest();
+object.onreadystatechange=function(){
+  if(this.readyState==4 && this.status==200){
+    var img=document.getElementById("myimg");
+    var title=document.getElementById("title");
+    var fname=document.getElementById("fname");
+      var gender=document.getElementById("gender");
+    var lname=document.getElementById("lname");
+    var city=document.getElementById("city");
+    var x=JSON.parse(this.responseText);
+    var url=x.results[0].picture.large;
+     fname.innerHTML=x.results[0].name.first;
+    lname.innerHTML=x.results[0].name.last;
+    title.innerHTML=x.results[0].name.title;
+    gender.innerHTML=x.results[0].gender;
+    city.innerHTML=x.results[0].location.city;
+    img.src=url;
+    
+    
+  }
+}
+object.open('GET','https://randomuser.me/api',true);
+object.send();
+}
